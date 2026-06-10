6345733aaf767b852ae170b481503796c45a7323
MCDLCN <martin.desbois@le-campus-numerique.fr>
2026-06-10 09:35:45 +0200

    Add first normal item test

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void normalItemQualityDecreasesByOne() {
+        Item[] items = new Item[] { new Item("Pain", 10, 20) };
+
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
-    }
 
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(19, app.items[0].quality);
+    }
 }
